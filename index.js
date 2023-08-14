e44fe0b2863b334bee1dc41a467be0ad46d66b59
Fabian Miranda <fabo.0220@gmail.com>
2023-08-13 22:13:39 -0500

    Responsive Pages

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function redirectToHome() {
-    window.location.href = "pages/home.html";
-  }
+  window.location.href = "pages/home.html";
+}
+
+document.getElementById("menu-icon").addEventListener("click", () => {
+  document.getElementById("nav").classList.toggle("active");
+});
